84122b3b2d448301c36fd5304be524f6036c6c42
Wulakey Tanwar <113882113+wulakey-tanwar@users.noreply.github.com>
2024-10-21 21:37:33 +0530

    Add files via upload

---
+++ pythonapp/src/myapp.py
@@ -0,0 +1,6 @@
+print("This is app to sum 2 number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter first number: "))
+
+result = num1 + num2
+print(f'Sum of 2 numbers are: {result}')
